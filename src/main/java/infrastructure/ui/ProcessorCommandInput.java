c4f07e7a3529a614d48c1930f71fdbc1d4eae971
pedrocan <pedro.candocia@gmail.com>
2022-01-31 18:47:03 +0100

    Simple Refactor processor input

--- src/main/java/infrastructure/ui/ProcessorCommandInput.java
+++ src/main/java/infrastructure/ui/ProcessorCommandInput.java
@@ -15,55 +15,78 @@ public final class ProcessorCommandInput {
     private static final String FOLLOWS_COMMAND = "follows";
     private static final String WALL_COMMAND    = "wall";
 
+    //For demo injection
     static final InMemoryTweetRepository inMemoryTweetRepository = new InMemoryTweetRepository();
     static final InMemoryUserRepository  inMemoryUserRepository  = new InMemoryUserRepository();
 
     public static void process(String input){
+
+        Matcher regexMatcher = prepareCommandRegExp(input);
+
+        executeCommandReadTweets(input, regexMatcher);
+
+        executeCommandWithParams(regexMatcher);
+    }
+
+    private static Matcher prepareCommandRegExp(String input) {
+
         //Command detect regexp
         //^([a-zA-Z]+)\s(\bwall\b|\bfollows\b|->)(\s.*)
 
         Pattern regex = Pattern.compile("^([a-zA-Z]+)\\s(\\bwall\\b|\\bfollows\\b|->)(.*)",
                 Pattern.CASE_INSENSITIVE | Pattern.MULTILINE );
 
-        Matcher regexMatcher = regex.matcher(input);
+        return regex.matcher(input);
 
-        String user = "", command = "", text = "" ;
+    }
 
-        if(regexMatcher.find()){
+    private static void executeCommandReadTweets(String input, Matcher regexMatcher) {
+
+        if(!regexMatcher.find()) {
+
+            //LAUNCH READ OWN TWEETS
+            AllTweetsByUserSearcher allTweetsByUserSearcher = new AllTweetsByUserSearcher(inMemoryTweetRepository);
+            List<Tweet>             tweetList               = allTweetsByUserSearcher.searchAll(input);
+            ConsolePrinterTweets.printPrettier(tweetList);
+
+        }
+    }
+
+    private static void executeCommandWithParams(Matcher regexMatcher) {
+
+        String user;
+        String command;
+        String text;
+
+        if (regexMatcher.groupCount() == 3) {
 
             user = regexMatcher.group(1);
             command = regexMatcher.group(2);
             text = regexMatcher.group(3).replaceFirst(" ", "");
 
-        }else{
-            user = input;
-            //LAUNCH READ OWN TWEETS
-            AllTweetsByUserSearcher allTweetsByUserSearcher = new AllTweetsByUserSearcher(inMemoryTweetRepository);
-            List<Tweet>             tweetList               = allTweetsByUserSearcher.searchAll(user);
-            ConsolePrinterTweets.printPrettier(tweetList);
+            //Extract pattern
+            if (POST_COMMAND.equals(command)) {
 
-        }
+                try {
 
-        if(POST_COMMAND.equals(command)){
+                    PostTweet postTweet = new PostTweet(inMemoryTweetRepository);
+                    postTweet.post(user, text);
 
-            try {
+                } catch (IllegalArgumentException exception) {
+                    System.err.println(exception.getMessage());
+                }
 
-                PostTweet postTweet = new PostTweet(inMemoryTweetRepository);
-                postTweet.post(user, text);
-
-            }catch (IllegalArgumentException exception){
-                System.err.println(exception.getMessage());
             }
+            if (FOLLOWS_COMMAND.equals(command)) {
+                FollowUser followUser = new FollowUser(inMemoryUserRepository);
+                followUser.follow(user, text);
+            }
+            if (WALL_COMMAND.equals(command)) {
+                WallTweets  wallTweets = new WallTweets(inMemoryTweetRepository, inMemoryUserRepository);
+                List<Tweet> tweetList  = wallTweets.wall(user);
+                ConsolePrinterTweets.printPrettier(tweetList);
 
-        }
-        if(FOLLOWS_COMMAND.equals(command)){
-            FollowUser followUser = new FollowUser(inMemoryUserRepository);
-            followUser.follow(user, text);
-        }
-        if(WALL_COMMAND.equals(command)){
-            WallTweets  wallTweets = new WallTweets(inMemoryTweetRepository,inMemoryUserRepository);
-            List<Tweet> tweetList  = wallTweets.wall(user);
-            ConsolePrinterTweets.printPrettier(tweetList);
+            }
 
         }
     }
